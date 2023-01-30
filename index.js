d57e4d2394ad5121641c6a1b7fec0402c5d7ef85
rosie.maguire <rosie.maguire@xplortechnologies.com>
2023-01-31 09:23:03 +1000

    Replaced JS with JQ & added delayed fade out/in

--- index.js
+++ index.js
@@ -1,17 +1,37 @@
-document.getElementById("sullyhead").addEventListener("click", function () {
-  console.log(document.getElementById("sullyhead").getAttribute("src"));
+// document.getElementById("sullyhead").addEventListener("click", function () {
+//   console.log(document.getElementById("sullyhead").getAttribute("src"));
+//   if (
+//     document.getElementById("sullyhead").getAttribute("src") ===
+//     "images/Happy Sully.png"
+//   ) {
+//     document
+//       .getElementById("sullyhead")
+//       .setAttribute("src", "images/Krish.jpg");
+//     document.getElementById("sullyhead").setAttribute("alt", "KrishTheBish");
+//   } else {
+//     document
+//       .getElementById("sullyhead")
+//       .setAttribute("src", "images/Happy Sully.png");
+//     document.getElementById("sullyhead").setAttribute("alt", "Happy Sully");
+//   }
+// });
+$("#sullyhead").on("click", function() {
+ 
   if (
-    document.getElementById("sullyhead").getAttribute("src") ===
-    "images/Happy Sully.png"
+    $("#sullyhead").attr("src") === "images/Happy Sully.png"
   ) {
-    document
-      .getElementById("sullyhead")
-      .setAttribute("src", "images/Krish.jpg");
-    document.getElementById("sullyhead").setAttribute("alt", "KrishTheBish");
+    $("#sullyhead").fadeOut();
+    setTimeout(function() {
+      $("#sullyhead").attr("src", "images/Krish.jpg");
+      $("#sullyhead").attr("alt", "Krish the Bish");
+      $("#sullyhead").fadeIn();
+    }, 250);
   } else {
-    document
-      .getElementById("sullyhead")
-      .setAttribute("src", "images/Happy Sully.png");
-    document.getElementById("sullyhead").setAttribute("alt", "Happy Sully");
+    $("#sullyhead").fadeOut();
+    setTimeout(function() {
+      $("#sullyhead").attr("src", "images/Happy Sully.png");
+      $("#sullyhead").attr("alt", "Happy Sully");
+      $("#sullyhead").fadeIn();
+    }, 250);
   }
 });
